25394014eab38a5d65321fe2b43492ea818c1c62
AlexandrCaftanat <joeykipelov@gmail.com>
2022-03-08 14:45:20 -0800

    Class Cache final core

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use ishop\App;
+use ishop\Cache;
 
 class MainController extends AppController {
 
@@ -15,6 +16,12 @@ class MainController extends AppController {
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание', 'Ключивики');
         $age = 32;
         $name = 'Александр';
+//        $lang = ['java', 'php', 'c#', 'javascript'];
+//        $cache = Cache::instance();
+//        $cache->set('test', $lang);
+//        $data = $cache->get('test');
+//        $cache->delete('test');
+//        debug($data);
         $this->set(compact('name','age', 'posts'));
 
     }
